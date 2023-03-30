c65454750dc9fa89fcacc802e03110c2f06b81a9
mk2408 <70700654+Mitali8620@users.noreply.github.com>
2023-03-30 21:40:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# google_maps_app
+
+A new Flutter project.
+
+## Getting Started
+
+## This project is a starting point for a Flutter application.
+This flutter app in integrate  a Google maps and added the some properties you can modify with your needs
+
+## Flutter version :
+Version :  3.7.7
---
+++ lib/home_page.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final LatLng setInitialPosition =
+      LatLng(21.17386991096315, 72.8298368265714);
+
+  final Set<Marker> _markers = {};
+
+  LatLng _lastMapPosition = setInitialPosition;
+
+  MapType _currentMapType = MapType.normal;
+
+  void _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  void _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: const InfoWindow(
+          title: 'Best place',
+          snippet: '',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Maps Sample App'),
+        backgroundColor: Colors.green[700],
+      ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: setInitialPosition,
+              zoom: 11.0,
+            ),
+            // mapType: _currentMapType,
+            mapType: MapType.normal,
+            markers: _markers,
+            onCameraMove: _onCameraMove,
+            myLocationEnabled: true,
+            buildingsEnabled: true,
+            compassEnabled: true,
+            trafficEnabled: true,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Column(
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: _onMapTypeButtonPressed,
+                    materialTapTargetSize: MaterialTapTargetSize.padded,
+                    backgroundColor: Colors.green,
+                    child: const Icon(Icons.map, size: 36.0),
+                  ),
+                  const SizedBox(height: 16.0),
+                  FloatingActionButton(
+                    onPressed: _onAddMarkerButtonPressed,
+                    materialTapTargetSize: MaterialTapTargetSize.padded,
+                    backgroundColor: Colors.green,
+                    child: const Icon(Icons.add_location, size: 36.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
